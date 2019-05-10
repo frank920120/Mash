e22b273eb0643d35524749e4cb2f0aac73187845
Naiji Zhang <naijizhang@gmail.com>
2019-05-10 11:55:27 -0700

    Add get user information API and filter user API

--- Mash/imports/api/artists.js
+++ Mash/imports/api/artists.js
@@ -1,8 +1,19 @@
 import { Meteor } from "meteor/meteor";
 export const Artists = Meteor.users;
+
+/**
+ * Subscribe profile
+ *  Meteor.subscribe('profile');
+ */
+if (Meteor.isServer) {
+  Meteor.publish("profile", function profilePublication() {
+    return Artists.find({ _id: this.userId }, { services: 0 });
+  });
+}
+
 /**
  *  Updating profiles
- *  Input: user{
+ *  Input: Object user{
  *    description:'',
  *    location:{
  *        lat:...,
@@ -14,61 +25,95 @@ export const Artists = Meteor.users;
  *      musicWorks:[]}
  *  Call:  Meteor.call('artists.updateProfile',user);
  */
-  Meteor.methods({
-    'artists.updateProfile'(user) {
-        if (!this.userId) {
-          throw new Meteor.Error(
-           "Please Login."
-          );
+Meteor.methods({
+  "artists.updateProfile"(user) {
+    if (!this.userId) {
+      throw new Meteor.Error("Please Login.");
+    }
+    Artists.update(
+      { _id: user._id },
+      {
+        $set: {
+          ...user
         }
-        Artists.update({_id:user._id}, {
-          $set: {
-           ...user
-          }
-        });
+      }
+    );
+  }
+});
+
+/**
+ * Get user information contain user name
+ * Input: String userName
+ * Call:  Meteor.call('artists.getProfileContainName', 'userName');
+ */
+Meteor.methods({
+  "artists.getProfileContainName"(userName) {
+    if (!this.userId) {
+      throw new Meteor.Error("Please Login.");
+    }
+    find({ _id: { $regex: ".*o.*" } });
+    return Artists.find(
+      { fullname: { $regex: `.*${userName}.*` } },
+      { services: 0 }
+    );
+  }
+});
+
+/**
+ * Get user information by user ID
+ * Input: String userId
+ * Call:  Meteor.call('artists.getProfileById', 'userId');
+ */
+Meteor.methods({
+  "artists.getProfileById"(userId) {
+    if (!this.userId) {
+      throw new Meteor.Error("Please Login.");
+    }
+    return Artists.find({ _id: userId }, { emails: 0, services: 0 });
+  }
+});
+
+/**
+ * Get user information by user specialties
+ * Input: Object filter{
+ *        specialties:[],
+ *        genre:[],
+ *      }
+ * Call:  Meteor.call('artists.getProfileByFilter', 'userId');
+ */
+Meteor.methods({
+  "artists.getProfileByFilter"(filter) {
+    if (!this.userId) {
+      throw new Meteor.Error("Please Login.");
+    }
+    return Artists.find(
+      {
+        specialties: { $all: filter.specialties },
+        genre: { $all: filter.genre }
       },
-  });
-
- /**
-  * Get user information by user name
-  */
-
-
- /**
-  * Get user by filter {specialties,genre,musicWorks number, review number, distance}
-  */
-
-
-  /**
-  * Get user by filter {specialties}
-  */
+      { emails: 0, services: 0 }
+    );
+  }
+});
+/**
+ * Get users by filter {specialties,genre,musicWorks number, review number, distance}
+ */
 
 
 
-  /**
-  * Get user by filter {genre}
-  */
+/**
+ * Get user by filter {musicWorks number}
+ */
 
+/**
+ * Get user by filter {review number}
+ */
 
+/**
+ * Get user by filter {distance}
+ */
 
-  /**
-  * Get user by filter {musicWorks number}
-  */
-
-
-  /**
-  * Get user by filter {review number, distance}
-  */
-
-
-
-  /**
-  * Get user by filter {distance}
-  */
-
-
-  /**
-   * 
-  * Get all user Locations by filter {distance}
-  */
-
+/**
+ *
+ * Get all user Locations by filter {distance}
+ */
