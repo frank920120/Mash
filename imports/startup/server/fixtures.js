7b8e899b6991a08abc316743b19eb7b6bd5cd785
Naiji Zhang <naijizhang@gmail.com>
2019-05-13 15:55:41 -0700

    Add Collaborations default data

--- imports/startup/server/fixtures.js
+++ imports/startup/server/fixtures.js
@@ -1,44 +1,60 @@
 import { Meteor } from "meteor/meteor";
 import { Artists } from "../../api/artists";
+import { Collaborations } from "../../api/collaborations";
 Meteor.startup(() => {
   if (Artists.find().count() === 0) {
     Artists.insert({
-        fullname: 'Alice',
-        email:'alice@red.com',
-        imageurl:'https://loremflickr.com/320/240',
-        description: 'Hi I am a singer.',
-        location:{
-            lat:null,
-            lng:null
-        }, 
-        reviews: [{
-            text: 'Good Job!',
-            reviewer: 'Bob'
-        }],
-       //ie: guitar, drums 
-        specialties: ['singing','dancing','b-box'],
-        //ie: rock, classical 
-        genre: ['rock'],
-        musicWorks:[]
+      fullname: "Alice",
+      email: "alice@red.com",
+      imageurl: "https://loremflickr.com/320/240",
+      description: "Hi I am a singer.",
+      location: {
+        lat: null,
+        lng: null
+      },
+      reviews: [
+        {
+          text: "Good Job!",
+          reviewer: "Bob"
+        }
+      ],
+      //ie: guitar, drums
+      specialties: ["singing", "dancing", "b-box"],
+      //ie: rock, classical
+      genre: ["rock"],
+      musicWorks: []
     });
     Artists.insert({
-        fullname: 'Bob',
-        description: 'Hi I am a drumer.',
-        imageurl:'https://loremflickr.com/320/240',
-        email:'bob@red.com',
-        location:{
-            lat:null,
-            lng:null
-        }, 
-        reviews: [{
-            text: 'Unbelievable!',
-            reviewer: 'Alice'
-        }],
-       //ie: guitar, drums 
-        specialties: ['drums','dancing','reading'],
-        //ie: rock, classical 
-        genre: ['rock'],
-        musicWorks:[]
+      fullname: "Bob",
+      description: "Hi I am a drumer.",
+      imageurl: "https://loremflickr.com/320/240",
+      email: "bob@red.com",
+      location: {
+        lat: null,
+        lng: null
+      },
+      reviews: [
+        {
+          text: "Unbelievable!",
+          reviewer: "Alice"
+        }
+      ],
+      //ie: guitar, drums
+      specialties: ["drums", "dancing", "reading"],
+      //ie: rock, classical
+      genre: ["rock"],
+      musicWorks: []
+    });
+  }
+  if (Collaborations.find().count() === 0) {
+    Collaborations.insert({
+      musicurl: "https://soundcloud.com/stormzzzzz/skrrt"
+    });
+    Collaborations.insert({
+      musicurl: "https://soundcloud.com/stormzzzzz/skrrt"
+    });
+    Collaborations.insert({
+      musicurl: "https://soundcloud.com/stormzzzzz/skrrt"
     });
   }
 });
