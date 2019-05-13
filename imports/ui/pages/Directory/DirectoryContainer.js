418a5f75c7c882cae9ef912d7813c9b5a2e03bb4
Andre Moura <andremarquesmoura@gmail.com>
2019-05-13 16:20:10 -0700

    Connect DirectoryContainer to backend

--- imports/ui/pages/Directory/DirectoryContainer.js
+++ imports/ui/pages/Directory/DirectoryContainer.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { Component } from "react";
+import ArtistCard from "../../component/ArtistCard/ArtistCard";
+import { Meteor } from "meteor/meteor";
+import { withTracker } from "meteor/react-meteor-data";
+import { Artists } from "../../../api/artists";
 
-const DirectoryContainer = () => {
-  return <div>This is DirectoryContainer page</div>;
-};
+class DirectoryContainer extends Component {
+  render() {
+    console.log(this.props);
+    return (
+      <div>
+        <div>
+          <p>This is DirectoryContainer</p>
+        </div>
+        {/* <ArtistCard /> */}
+      </div>
+    );
+  }
+}
 
-export default DirectoryContainer;
+export default withTracker(() => {
+  Meteor.subscribe("allusers");
+  return {
+    allUsers: Artists.find({}).fetch()
+  };
+})(DirectoryContainer);
