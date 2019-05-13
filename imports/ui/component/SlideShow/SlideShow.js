293f8143d52758ce91cf0213b3f0cb7613cbe9b2
Naiji Zhang <naijizhang@gmail.com>
2019-05-13 15:56:46 -0700

    Update slide show music url

--- imports/ui/component/SlideShow/SlideShow.js
+++ imports/ui/component/SlideShow/SlideShow.js
@@ -3,32 +3,6 @@ import Coverflow from "react-coverflow";
 import styles from "./styles";
 import { withStyles } from "@material-ui/core/styles";
 import MediaCard from "../../component/MediaCard";
-// const media = [
-//   {
-//     title: "aaa",
-//     author: "aaa",
-//     imageurl: "https://loremflickr.com/320/240",
-//     musicurl: ""
-//   },
-//   {
-//     title: "bbb",
-//     author: "bbbb",
-//     imageurl: "https://loremflickr.com/320/240",
-//     musicurl: ""
-//   },
-//   {
-//     title: "ccc",
-//     author: "ccccc",
-//     imageurl: "https://loremflickr.com/320/240",
-//     musicurl: ""
-//   },
-//   {
-//     title: "ddd",
-//     author: "dddddd",
-//     imageurl: "https://loremflickr.com/320/240",
-//     musicurl: ""
-//   }
-// ];
 const SlideShow = ({ classes, media }) => (
   <Coverflow
     className={classes.root}
@@ -40,12 +14,8 @@ const SlideShow = ({ classes, media }) => (
   >
     {media.map(music => (
       <MediaCard
-        title={music.title}
-        author={music.author}
-        imageurl={music.imageurl}
-        musicurl={music.musicurl}
         clientId='nJEgYbVDhjykb4wrNgX7LS7PM82wc0KI'
-        resolveUrl={music.musicurl}
+        resolveUrl={music}
       />
     ))}
   </Coverflow>
