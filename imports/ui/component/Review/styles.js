1b9e5c1c40c01e4c18411f50e38af52a0921f343
Nancy Chu <nancy.q.chu@gmail.com>
2019-05-13 16:32:17 -0700

    add title, review and reviewer text to Reviews component

---
+++ imports/ui/component/Review/styles.js
@@ -0,0 +1,27 @@
+const styles = theme => ({
+  reviewCard: {
+    margin: 10,
+    display: "flex",
+    flexDirection: "column"
+  },
+  review: {
+    fontStyle: "italic",
+    margin: 2,
+    padding: 5,
+    backgroundColor: "white",
+    opacity: "0.5",
+    borderRadius: 4
+  },
+
+  reviewer: {
+    alignSelf: "flex-end"
+  },
+  whiteText: {
+    color: "white"
+  },
+  h4: {
+    margin: 3
+  }
+});
+
+export default styles;
