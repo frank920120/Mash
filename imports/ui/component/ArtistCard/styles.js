9e5dec6d46cbb3575731b52ff3e9228cad2847a1
Andre Moura <andremarquesmoura@gmail.com>
2019-05-14 11:28:39 -0700

    Style the Directory page container

--- imports/ui/component/ArtistCard/styles.js
+++ imports/ui/component/ArtistCard/styles.js
@@ -1,12 +1,52 @@
 const styles = {
   card: {
-    maxWidth: 345
+    marginTop: 100,
+    margin: "5%",
+    padding: 10,
+    //maxWidth: "100%",
+    borderStyle: "none",
+    // backgroundColor: "#0E0B16"
+    background:
+      "linear-gradient(180deg, #0E0B16 0%, rgba(255, 255, 255, 0) 100%), #4717F6;"
   },
   media: {
-    height: 500,
+    height: 300,
     width: 300,
     borderRadius: "50%"
+  },
+  typography: {
+    color: "white"
+  },
+  button: {
+    marginTop: 30
+  },
+  avatar: {
+    width: 230,
+    height: 230
   }
 };
 
 export default styles;
+
+/*
+const styles = {
+  picButton: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  artistInfo: {
+    marginTop: 40
+  },
+  genreChip: {
+    margin: 10
+  },
+  specialtyChip: {
+    margin: 10
+  }
+};
+
+export default styles;
+
+*/
---
+++ imports/ui/pages/Directory/styles.js
@@ -0,0 +1,10 @@
+const styles = {
+  root: {
+    display: "flex",
+    flexGrow: 1,
+    justifyContent: "center",
+    backgroundColor: "#0E0B16"
+  }
+};
+
+export default styles;
