8c9b8c929e646a08af70e705859122961aec780e
Frank Fang <frankfang2014@hotmail.com>
2019-05-16 11:24:42 -0700

    fix specialties

--- imports/ui/component/HomeMap/HomeMap.js
+++ imports/ui/component/HomeMap/HomeMap.js
@@ -94,12 +94,12 @@ class HomeMap extends Component {
                 <Marker
                   key={user._id}
                   onClick={this.onMarkerClick}
-                  name={user.fullname}
-                  specialties={user.specialties}
+                  name={user.profile.fullname}
+                  specialties={user.profile.specialties}
                   id={user._id}
                   position={{
-                    lat: user.location.lat,
-                    lng: user.location.lng
+                    lat: user.profile.location.lat,
+                    lng: user.profile.location.lng
                   }}
                 />
               ))}
