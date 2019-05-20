819d1c91529de58f7c51db528587d72e9bcb8013
Naiji Zhang <naijizhang@gmail.com>
2019-05-20 15:59:48 -0700

    Remove extra commends and add login check on serverside

--- imports/api/artists.js
+++ imports/api/artists.js
@@ -20,35 +20,12 @@ if (Meteor.isServer) {
     return Artists.find({}, { emails: 0, services: 0 });
   });
 }
-// import { withTracker } from "meteor/react-meteor-data";
-// import {Artists} from "../../../api/artists";
-// export default withTracker(() => {
-//   Meteor.subscribe('allusers');
-//   return {
-//     allUsers: Artists.find({}).fetch(),
-//   };
-// })(App);
 
-/**
- *  Updating profiles
- *  Input: Object user{
- *    description:'',
- *    imageurl:'',
- *    location:{
- *        lat:...,
- *        lng:...
- *     },
- *     reviews: ['','',....],
- *     specialties:[],
- *      genre:[],
- *      musicWorks:[],}
- *  Call:  Meteor.call('artists.updateProfile',user);
- */
 Meteor.methods({
   "artists.updateProfile"(user) {
-    // if (!this.userId) {
-    //   throw new Meteor.Error("Please Login.");
-    // }
+    if (!this.userId) {
+      throw new Meteor.Error("Please Login.");
+    }
     Artists.update(
       { _id: user._id },
       {
@@ -59,24 +36,6 @@ Meteor.methods({
     );
   }
 });
-
-/**
- * Get user information contain user name
- * Input: String userName
- * Call:  Meteor.call('artists.getProfileContainName', 'userName');
- */
-// Meteor.methods({
-//   "artists.getProfileContainName"(userName) {
-//     if (!this.userId) {
-//       throw new Meteor.Error("Please Login.");
-//     }
-//     find({ _id: { $regex: ".*o.*" } });
-//     return Artists.find(
-//       { fullname: { $regex: `.*${userName}.*` } },
-//       { services: 0 }
-//     ).fetch();
-//   }
-// });
 
 /**
  * Get user information by user ID
@@ -91,39 +50,16 @@ if (Meteor.isServer) {
 }
 
 /**
- * Get all user Locations
- *
- */
-// Meteor.methods({
-//   "artists.getLocationsByFilter"() {
-//     if (!this.userId) {
-//       throw new Meteor.Error("Please Login.");
-//     }
-//     return Artists.find({}, { location: 1 }).fetch();
-//   }
-// });
-
-/**
  * Get all users
  */
 Meteor.methods({
   "artists.getAllUsers"() {
-    // if (!this.userId) {
-    //   throw new Meteor.Error("Please Login.");
-    // }
+    if (!this.userId) {
+      throw new Meteor.Error("Please Login.");
+    }
     return Artists.find({}, { emails: 0, services: 0 }).fetch();
   }
 });
-//Example:
-// import { Meteor } from "meteor/meteor";
-// Meteor.call("artists.getAllUsers", null, (err, res) => {
-//   if (err) {
-//     console.log("Error", err);
-//   } else {
-//     // success!
-//     console.log("res", res);
-//   }
-// });
 
 //get the filter
 if (Meteor.isServer) {
@@ -161,35 +97,12 @@ if (Meteor.isServer) {
     return Artists.find(query, { emails: 0, services: 0 });
   });
 }
-/**
- * example for filter users
- */
-// import { Meteor } from "meteor/meteor";
-// import { Artists } from "../../../api/artists";
-// export default withTracker(() => {
-//   const filter = {
-//     fullname: "Ali",  //find the name  contain the string
-//     specialties:['dancing'],  // find specialties contain all strings in the array
-//     genres:['rock'] // find genres contain all strings in the array
-//   };
-//   Meteor.subscribe("filterUsers", filter);
-//   return {
-//     filteredusers: Artists.find({}).fetch()
-//   };
-// })(ComponentName);
 
-//for sending message
-// const message = {
-//   toId: '',
-//   text: '',
-//   fromId: '',
-//   from:''
-// };
 Meteor.methods({
   "artists.addMessage"(message) {
-    // if (!this.userId) {
-    //   throw new Meteor.Error("Please Login.");
-    // }
+    if (!this.userId) {
+      throw new Meteor.Error("Please Login.");
+    }
     console.log(message);
     Artists.update(
       { _id: message.toId },
@@ -207,15 +120,11 @@ Meteor.methods({
   }
 });
 
-// friend={
-//   idA:'',
-//   idB:''
-// }
 Meteor.methods({
   "artists.addFriend"(friend) {
-    // if (!this.userId) {
-    //   throw new Meteor.Error("Please Login.");
-    // }
+    if (!this.userId) {
+      throw new Meteor.Error("Please Login.");
+    }
     Artists.update(
       { _id: friend.idA },
       {
@@ -253,9 +162,9 @@ Meteor.methods({
 
 Meteor.methods({
   "artists.removeNewFriendAlert"() {
-    // if (!this.userId) {
-    //   throw new Meteor.Error("Please Login.");
-    // }
+    if (!this.userId) {
+      throw new Meteor.Error("Please Login.");
+    }
     Artists.update(
       { _id: this.userId },
       {
